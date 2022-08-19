e10907dbf0674aafbef339a706a774a8158af7f2
smithy02 <106395507+smithy02@users.noreply.github.com>
2022-08-19 14:53:37 +0100

    Add files via upload

---
+++ javascript-course.js
@@ -0,0 +1,23 @@
+// when thr user clicks on <div>,openthe popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    // popup.classList.toggle("show");
+    popup.classList.toggle("show");
+}
+
+
+
+function myFunctionLeft() {
+   let popup2 = document.getElementById("myPopup2"); 
+    popup2.classList.toggle("show2");
+}
+function closePopup() {
+    // var popup2 = document.getElementById("myPopup2");
+    let popup2 = document.getElementById("myPopup2");
+    popup2.classList.add("show3");
+}
+
+function myFunctionLeftTwo() {
+   let popup3 = document.getElementById("myPopup3"); 
+    popup3.classList.toggle("show3");
+}
